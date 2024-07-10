26c2d92bc71e1a44b6bd241e5aeb570c94c07e48
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-07-10 20:57:25 +0600

    Update 01. Find out the largest between three numbers using conditional operator.c

--- 01. Find out the largest between three numbers using conditional operator.c
+++ 01. Find out the largest between three numbers using conditional operator.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main ()
+int main()
 {
-int a, b;
-printf("Enter a Number: ");
-scanf("%d", &a);
-(a%2==0)? printf("Even"):printf("Odd");
+int num1,num2,num3,largest;
+printf("Enter Three iInteger Number: ");
+scanf("%d %d %d",&num1,&num2,&num3);
+largest = (num1 > num2 && num1 > num3)? num1 : (num2 > num3)? num2 : num3;
+printf(" The Largest number is : %d\n",largest);
 return 0;
 }
